ff2e16e3ba3b0b5517fdbfaae7e7cedd9db4701a
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-12-11 10:24:36 +0100

    Feat: ajout de la route de recuperation de tous les objets

--- GestObjet/app/controllers/objets_controller.ts
+++ GestObjet/app/controllers/objets_controller.ts
@@ -1,10 +1,21 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import logger from '@adonisjs/core/services/logger'
+import Objet from '#models/objet'
 
 export default class ObjetsController {
   /**
    * Display a list of resource
    */
-  async getAll({}: HttpContext) {}
+  async getAll({ response }: HttpContext) {
+    try {
+      const objets: Objet[] = await Objet.all()
+
+      return response.status(200).send(objets)
+    } catch (err) {
+      logger.error({ err: err }, 'Erreur de recuperation des objets')
+      return response.status(500).send('Erreur serveur')
+    }
+  }
 
   /**
    * Handle form submission for the create action
